3d1bd31a9bd3c134b099b67b6790874bcaf7e7dc
Kenny Yoon <ghun.yoon@gmail.com>
2021-07-09 00:45:18 -0400

    done

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,23 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students
+
+  def add_student(name, age)
+    Student.create(name: name, age: age, cohort_id: self.id)
+  end
+
+  def average_age
+    self.students.average(:age).to_f
+  end
+
+  def total_students
+    self.students.count.to_i
+  end
+
+  def self.biggest
+    self.all.max_by {|c| c.students.count}
+  end
+
+  def self.sort_by_mod
+    Cohort.order(:current_mod)
+  end
 end
